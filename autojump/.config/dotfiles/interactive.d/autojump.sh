0bc8449de5ca41290529dc9e41bdf5ff4709814a
stv <stv@stanford.edu>
2018-03-03 22:05:26 -0800

    autojump

--- autojump/.config/dotfiles/interactive.d/autojump.sh
+++ autojump/.config/dotfiles/interactive.d/autojump.sh
@@ -6,6 +6,8 @@ if type brew >/dev/null 2>&1; then
     if [ -s $(brew --prefix)/etc/profile.d/autojump.sh ]; then
         . $(brew --prefix)/etc/profile.d/autojump.sh
     fi
+elif [ -s /usr/share/autojump/autojump.sh ]; then
+    . /usr/share/autojump/autojump.sh
 elif [ -s /usr/local/etc/autojump.sh ]; then
     . /usr/local/etc/autojump.sh
 fi
