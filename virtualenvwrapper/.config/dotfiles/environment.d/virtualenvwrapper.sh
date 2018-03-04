bcb85f92cf95629fa4d9b5b030d5ff0fc4278265
stv <stv@stanford.edu>
2018-03-03 22:11:33 -0800

    virtualenvwrapper

--- virtualenvwrapper/.config/dotfiles/environment.d/virtualenvwrapper.sh
+++ virtualenvwrapper/.config/dotfiles/environment.d/virtualenvwrapper.sh
@@ -2,9 +2,3 @@
 export WORKON_HOME=${HOME}/.local/virtualenvs
 export PROJECT_HOME=${HOME}/projects
 export VIRTUALENVWRAPPER_VIRTUALENV_ARGS='--no-site-packages'
-# if [ -s ${HOME}/local/bin/python ]; then
-    # . ${HOME}/local/bin/activate
-    # pip install virtualenv virtualenvwrapper
-    # deactivate
-    # export VIRTUALENVWRAPPER_PYTHON=${HOME}/local/bin/python
-# fi
