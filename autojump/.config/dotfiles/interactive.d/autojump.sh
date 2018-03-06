ad98a00b35b5271a024c940963cdbb253749fb4b
stv <stv@stanford.edu>
2018-03-05 20:49:40 -0800

    KEEP: don't migrate osx data

--- autojump/.config/dotfiles/interactive.d/autojump.sh
+++ autojump/.config/dotfiles/interactive.d/autojump.sh
@@ -2,12 +2,6 @@
 if [ -n "${debug}" ]; then
     echo 'funk:autojump:interactive'
 fi
-if type brew >/dev/null 2>&1; then
-    if [ -s $(brew --prefix)/etc/profile.d/autojump.sh ]; then
-        . $(brew --prefix)/etc/profile.d/autojump.sh
-    fi
-elif [ -s /usr/share/autojump/autojump.sh ]; then
-    . /usr/share/autojump/autojump.sh
-elif [ -s /usr/local/etc/autojump.sh ]; then
-    . /usr/local/etc/autojump.sh
+if [ -e ${HOME}/.local/etc/profile.d/autojump.sh ]; then
+    . ${HOME}/.local/etc/profile.d/autojump.sh
 fi
