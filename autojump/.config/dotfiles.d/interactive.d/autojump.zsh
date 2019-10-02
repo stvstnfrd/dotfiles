ef7ed7a1fd6205604f462919a90a00ef2cbd5359
stv <stv@stanford.edu>
2019-10-02 03:18:59 -0700

    autojump

--- autojump/.config/dotfiles.d/interactive.d/autojump.zsh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-if [ -n "${debug}" ]; then
-    echo 'funk:autojump:interactive:zsh'
-fi
-if [ -e ${HOME}/.local/share/autojump/autojump.zsh ]; then
-    . ${HOME}/.local/share/autojump/autojump.zsh
-fi
