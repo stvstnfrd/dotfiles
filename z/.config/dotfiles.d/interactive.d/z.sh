e7d270642a63b70afb93d283baf5ef8e210f8b03
stv <stv@stanford.edu>
2019-10-02 03:58:58 -0700

    z

--- z/.config/dotfiles.d/interactive.d/z.sh
+++ z/.config/dotfiles.d/interactive.d/z.sh
@@ -4,5 +4,23 @@
 # $_Z_NO_PROMPT_COMMAND to handle PROMPT_COMMAND/precmd your-self.
 # $_Z_EXCLUDE_DIRS to an array of directory trees to exclude.
 # $_Z_OWNER to allow usage when in 'sudo -s' mode.
-export _Z_DATA=${XDG_CACHE_HOME}/z
 . ${HOME}/src/z/z.sh
+function j(){
+    if [ ${#} -gt 0 ]; then
+        z ${@}
+    else
+        z "$(z -lt | awk '{print $2}' | tail -2 | head -1)"
+    fi
+}
+function jj(){
+    if [ ${#} -gt 0 ]; then
+        z -r ${@}
+    else
+        recent="$(z -l | awk '{print $2}' | tail -1)"
+        if [ "${recent}" = "$(pwd)" ]; then
+            z "$(z -l | awk '{print $2}' | tail -2 | head -1)"
+        else
+            z "${recent}"
+        fi
+    fi
+}
