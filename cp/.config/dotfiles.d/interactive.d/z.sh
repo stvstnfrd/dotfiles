febb14daeebd578d45a0b53e6a2c945e7580071e
stv <stv@stanford.edu>
2019-10-04 19:01:50 -0700

    z stuff

---
+++ cp/.config/dotfiles.d/interactive.d/z.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+CDPATH="${HOME}/src"
+CDPATH="${HOME}/src/edx:${CDPATH}"
+CDPATH="${HOME}/src/edx/xblocks:${CDPATH}"
+CDPATH="${HOME}/projects:${CDPATH}"
+_path=
+_name=.
+for i in {1..3}; do
+    _path="${_path}../"
+    _name="${_name}."
+    alias ${_name}="cd $_path && ls"
+done
