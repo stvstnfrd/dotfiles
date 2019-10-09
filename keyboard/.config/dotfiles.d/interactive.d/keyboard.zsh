9a468400f568f794b06c3205588214931df6b0db
stv <stv@stanford.edu>
2019-10-09 09:11:34 -0700

    keys

--- keyboard/.config/dotfiles.d/interactive.d/keyboard.zsh
+++ keyboard/.config/dotfiles.d/interactive.d/keyboard.zsh
@@ -1,12 +1,2 @@
 #!/bin/zsh
 bindkey -v
-bindkey '^P' up-history
-bindkey '^N' down-history
-# bindkey '^?' backward-delete-char
-bindkey '^h' backward-delete-char
-bindkey '^w' backward-kill-word
-bindkey '^r' history-incremental-search-backward
-bindkey '^a' beginning-of-line
-bindkey '^e' end-of-line
-bindkey '^f' forward-char
-bindkey '^b' backward-char
