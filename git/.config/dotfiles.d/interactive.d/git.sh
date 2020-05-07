04d6118657f1eb89df0766c40e783e197db8d719
stvn <stvn@mit.edu>
2020-05-07 07:13:04 -0700

    Only set up some helpers contextually

--- git/.config/dotfiles.d/interactive.d/git.sh
+++ git/.config/dotfiles.d/interactive.d/git.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # map aliases from git to bash
 # foreach git alias 'x', create bash alias 'gx'
-for a in $(git var -l | sed -nE 's/^alias\.([^=]*)=.*/\1/p'); do
-    alias g${a}="git ${a} "
-done
+if command -v git 2>&1 >/dev/null; then
+    for a in $(git var -l | sed -nE 's/^alias\.([^=]*)=.*/\1/p'); do
+        alias g${a}="git ${a} "
+    done
+fi
