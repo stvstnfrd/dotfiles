1e607ca157778f919a2cc29e2dd30696d317c412
stvn <stvn@mit.edu>
2020-10-06 22:50:36 -0700

    Create vagrant aliases iff installed

--- vagrant/.config/dotfiles.d/interactive.d/vagrant.sh
+++ vagrant/.config/dotfiles.d/interactive.d/vagrant.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-alias vh='vagrant halt '
-alias vha='vagrant-halt-all '
-alias vr='vagrant reload '
-alias vs='vagrant ssh '
-alias vu='vagrant up '
-alias vus='vagrant up; vagrant ssh '
+if command -v vagrant >/dev/null 2>&1; then
+    alias vh='vagrant halt '
+    alias vha='vagrant-halt-all '
+    alias vr='vagrant reload '
+    alias vs='vagrant ssh '
+    alias vu='vagrant up '
+    alias vus='vagrant up; vagrant ssh '
+fi
