54a452e35660e0565f5a6a0c47a6ee7f27059e72
stvn <stvn@mit.edu>
2020-12-04 16:14:00 -0800

    Install make/git during bootstrap, if needed

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 set -e
+if ! command -v make >/dev/null 2>&1; then
+    sudo apt install -y build-essential
+fi
+if ! command -v git >/dev/null 2>&1; then
+    sudo apt install -y git
+fi
 if [ ! -d "${HOME}/.config/dotfiles" ]; then
     git clone \
         https://github.com/stvstnfrd/dotfiles.git \
