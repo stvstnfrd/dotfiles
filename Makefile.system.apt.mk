9f7652b15f8202fd30f1b941b08264892917cb9e
stvn <stvn@mit.edu>
2021-07-31 01:51:57 -0700

    style(make): cleanup comment

--- Makefile.system.apt.mk
+++ Makefile.system.apt.mk
@@ -1,5 +1,5 @@
 #!/usr/bin/make -f
-# gfortran ## needed for scipy
+# gfortran needed for scipy
 APT_EXISTS=$(shell command -v apt 2>&1 >/dev/null && echo 1 || echo 0)
 ifeq ($(APT_EXISTS),1)
 APT_INSTALL ?= 1
