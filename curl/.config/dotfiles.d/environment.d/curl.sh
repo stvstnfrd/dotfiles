ed20dc4debb059ac44a5b9c01d24df8ccf4f967e
stvn <stvn@mit.edu>
2021-08-10 22:24:19 -0700

    refactor(xdg): move curl wrapper into common xdg script

--- curl/.config/dotfiles.d/environment.d/curl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-curl() {
-    _flags=
-    if [ -e "${NETRC}" ]; then
-        _flags="--netrc-file ${NETRC}"
-    fi
-    # shellcheck disable=SC2086
-    command curl ${_flags} "${@}"
-}
