f1e5a79f56e50c55d3707453d7a1febbce7e6f18
stvn <stvn@mit.edu>
2021-08-10 19:54:18 -0700

    build(lint)!: stop running lint.diff
    as its just a subset of the tests we're running anyway.
    The common case will be a failed build in both (new changes), so we're
    not really gaining much with it.

--- Makefile.lint.mk
+++ Makefile.lint.mk
@@ -36,27 +36,9 @@ FIND_FILES_ALL=( \
 	) \
 	| sort --zero-terminated --uniq \
 )
-FIND_FILES_DIFF_UNFILTERED=( \
-	git diff --name-only -z --diff-filter=AMd $(TRAVIS_BRANCH) HEAD \
-	| sort --zero-terminated --uniq \
-)
-FIND_FILES_DIFF=( \
-	( \
-		$(FIND_FILES_ALL); \
-		$(FIND_FILES_DIFF_UNFILTERED); \
-	) \
-	| sort --zero-terminated \
-	| uniq --zero-terminated --repeated \
-)
-LINT_SH_DIFF = $(FIND_FILES_DIFF) | $(XARGS_SHELLCHECK) --exclude=SC1091
 LINT_SH_ALL=$(FIND_FILES_ALL) | $(XARGS_SHELLCHECK)
 XARGS_SHELLCHECK = xargs -0 --no-run-if-empty shellcheck --external-sources
 
 .PHONY: lint
 lint:  ## Run the linter against all files
 	$(LINT_SH_ALL)
-
-TRAVIS_BRANCH ?= origin/master
-.PHONY: lint.diff
-lint.diff:  ## Run the linter against files changed since master
-	$(LINT_SH_DIFF)
