f640acf3c95cdb246d9d42e126106a28faabaa8e
stvn <stvn@mit.edu>
2021-08-10 21:55:23 -0700

    fix(harden): disable lightdm again

--- .requirements/configure.mk
+++ .requirements/configure.mk
@@ -2,4 +2,5 @@
 .PHONY: configure.harden
 configure.harden:  ## Harden local configuration
 	sudo systemctl disable lightdm.service || true
+	sudo systemctl disable lightdm || true
 	sudo ufw enable || true
