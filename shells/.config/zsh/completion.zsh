671f17334cdc127dd75cb518cea5410c3fe4b47e
stvn <stvn@mit.edu>
2021-09-09 15:54:48 -0700

    feat(zsh): stop completion mid-word

--- shells/.config/zsh/completion.zsh
+++ shells/.config/zsh/completion.zsh
@@ -9,7 +9,7 @@ autoload -U compinit
 unsetopt menu_complete   # do not autoselect the first completion entry
 unsetopt flowcontrol
 setopt auto_menu         # show completion menu on succesive tab press
-setopt complete_in_word
+unsetopt complete_in_word
 setopt always_to_end
 
 # Figure out the SHORT hostname
