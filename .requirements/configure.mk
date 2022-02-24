f245953ccf8a9e66c0e886c5560af825b20eb947
stvstnfrd <stvstnfrd@gmail.com>
2022-02-23 18:01:47 -0700

    refactor(build): reorder hardening steps
    since this should happen first/always anyway.
    It'll also make pending changes easier.

--- .requirements/configure.mk
+++ .requirements/configure.mk
@@ -1,6 +1,6 @@
 #!/usr/bin/make -f
 .PHONY: configure.harden
 configure.harden:  ## Harden local configuration
+	sudo ufw enable || true
 	sudo systemctl disable lightdm.service || true
 	sudo systemctl disable lightdm || true
-	sudo ufw enable || true
