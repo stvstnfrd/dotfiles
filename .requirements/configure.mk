d3caec6fef4007ac7372808af5b8021d8b1a4ad1
stvstnfrd <stvstnfrd@gmail.com>
2022-02-23 18:23:03 -0700

    feat(build): force text-mode upon startup

--- .requirements/configure.mk
+++ .requirements/configure.mk
@@ -4,3 +4,8 @@ configure.harden:  ## Harden local configuration
 	sudo ufw enable || true
 	sudo systemctl disable lightdm.service || true
 	sudo systemctl disable lightdm || true
+	sudo systemctl set-default multi-user.target || true
+	sudo sed -i.bak 's/^GRUB_CMDLINE_LINUX_DEFAULT=.*$/# &/' /etc/default/grub || true
+	echo 'GRUB_CMDLINE_LINUX_DEFAULT="text"' \
+	| sudo tee -a /etc/default/grub GRUB_CMDLINE_LINUX_DEFAULT="text" || true
+	sudo update-grub || true
