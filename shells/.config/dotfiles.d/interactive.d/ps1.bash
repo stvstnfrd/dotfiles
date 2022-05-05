9e54713fa64ce4ae0c014acaec8264b5600829a1
stvstnfrd <stvstnfrd@noreply.users.github.com>
2022-05-05 15:42:59 -0600

    feat(ps1): colorize contextually ${USER}@${HOST}:${PWD}

--- shells/.config/dotfiles.d/interactive.d/ps1.bash
+++ shells/.config/dotfiles.d/interactive.d/ps1.bash
@@ -5,7 +5,8 @@ PS1_SHELL() {
 
 PS1="
 
-# \$(PS1_EMOTICON) [\t]   [\u@\h:\w]
+# \$(PS1_EMOTICON) [\t]   [\$(PS1_HASH_COLOR ${USER})@\$(PS1_HASH_COLOR ${HOSTNAME}):\$(PS1_PATH)]
           \$(PS1_SHELL) \$(PS1_GIT) \$(PS1_VENV) \$(PS1_NODE)"
 PS1="${PS1}\n# "
 export PS1
+# vim: tw=1000
--- shells/.config/dotfiles.d/interactive.d/ps1.sh
+++ shells/.config/dotfiles.d/interactive.d/ps1.sh
@@ -50,9 +50,35 @@ else
 		fi
 	) }
 
+	PS1_PATH() {
+		PS1_HASH_COLOR "$(
+			echo "${PWD}" \
+			| sed 's@/home/[^/]\+@~@'
+		)"
+	}
+
 	PS1_VENV() { (
 		if [ -n "${VIRTUAL_ENV}" ]; then
 			echo "[venv:$(basename "${VIRTUAL_ENV}")]"
 		fi
 	) }
+
+	# cat /usr/share/dict/american-english | while read -r word; do printf '%s\n' "$(PS1_HASH_COLOR "${word}")"; sleep .25s; done
+	PS1_HASH_COLOR() {
+		text="${1}"
+		hash="$(
+			printf '%s' "${text}" \
+			| md5sum \
+			| awk '{print $1}' \
+			| tr -d '\n' \
+			| tail -c2 \
+			| tr '[:lower:]' '[:upper:]' \
+		)"
+		hash="$(echo "ibase=16; ${hash} % FF" | bc)"
+		color_hash="\e[${hash};1m"
+		color_hash="\e[38;5;${hash}m"
+		color_stop='\e[0m'
+		output="${color_hash}${text}${color_stop}"
+		printf "%b" "${output}"
+	}
 fi
