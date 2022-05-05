58a2ceeb24fe116f38da3ebf348e0787fd485409
stvstnfrd <stvstnfrd@noreply.users.github.com>
2022-05-04 18:46:18 -0600

    feat(ufw): add basic firewall rules

--- .requirements/configure.mk
+++ .requirements/configure.mk
@@ -2,6 +2,9 @@
 .PHONY: configure.harden
 configure.harden:  ## Harden local configuration
 	sudo ufw enable || true
+	sudo ufw allow ssh comment 'ssh' || true
+	sudo ufw allow proto tcp from 192.168.0.0/24 port 24800 to 192.168.0.0/24 port 24800 comment 'barrier-kvm' || true
+	sudo ufw allow proto tcp from port 8123 to port 8123 comment 'home-assistant' || true
 	sudo systemctl disable lightdm.service || true
 	sudo systemctl disable lightdm || true
 	sudo systemctl set-default multi-user.target || true
