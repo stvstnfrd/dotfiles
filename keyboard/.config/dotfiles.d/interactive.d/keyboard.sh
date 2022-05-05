e29043f180f30df9c047a356b15b02e4d9b91fe4
stvstnfrd <stvstnfrd@noreply.users.github.com>
2022-05-05 14:57:20 -0600

    feat(x11): add keyboard remappings for x11 sessions

--- keyboard/.config/dotfiles.d/interactive.d/keyboard.sh
+++ keyboard/.config/dotfiles.d/interactive.d/keyboard.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 set -o vi
 export INPUTRC="${XDG_CONFIG_HOME}/readline/inputrc"
-if [ -n "${DISPLAY}" ]
-then
-    if command -v setxkbmap >/dev/null 2>&1
-    then
-        setxkbmap -option caps:swapescape
-    fi
-fi
