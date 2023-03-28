5194883be605c620db734089607941c23ffa92f5
stvstnfrd <stvstnfrd@noreply.users.github.com>
2023-03-28 17:34:55 -0600

    refactor: inline other package lists

--- .requirements/system.pip.mk
+++ .requirements/system.pip.mk
@@ -1,6 +1,6 @@
 #!/usr/bin/make -f
 PIP_EXISTS=$(shell command -v pip 2>&1 >/dev/null && echo 1 || echo 0)
-PIP_PACKAGES=$(shell grep -v '^\#' .requirements/pip.txt)
+PIP_PACKAGES ?= base58 ptpython
 
 .PHONY: system.pip
 system.pip:  ## Install pip packages
